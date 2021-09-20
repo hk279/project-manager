3a2dea67b5332ac35f42a70c33576796199bdc83
Jere Järvinen <AA5536@student.jamk.fi>
2021-09-20 20:36:51 +0300

    Implemented project file attachments delete and refined the file list. Added messages on successful upload/delete or error

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,22 +1,75 @@
-import React from "react";
-import { Upload, Button } from "antd";
-import { UploadOutlined } from "@ant-design/icons";
+import React, { useState } from "react";
+import { Upload, Button, Modal, message } from "antd";
+import { UploadOutlined, ExclamationCircleOutlined } from "@ant-design/icons";
 import URLroot from "../config/config";
+import axios from "axios";
 
 const FileUpload = ({ projectId, files }) => {
+    const { confirm } = Modal;
+
+    const initialFileList = files.map((file) => ({
+        uid: file.fileKey,
+        name: file.fileName,
+        url: `${URLroot}/projects/get-file/${file.fileKey}`,
+        status: "done",
+    }));
+
+    const [fileList, setFileList] = useState(initialFileList);
+
+    const showConfirm = (file) => {
+        return new Promise((resolve, reject) => {
+            confirm({
+                title: `Permanently delete '${file.name}'?`,
+                icon: <ExclamationCircleOutlined />,
+                onOk() {
+                    axios
+                        .put(`${URLroot}/projects/${projectId}/delete-file/${file.uid}`)
+                        .then(() => message.info(`'${file.name}' deleted successfully`))
+                        .catch((err) => {
+                            console.log(err);
+                            message.error(`${file.name} delete failed`);
+                        });
+                    resolve(true);
+                },
+                onCancel() {
+                    reject(true);
+                },
+            });
+        });
+    };
+
+    const handleChange = (info) => {
+        if (info.file.status === "done") {
+            message.success(`${info.file.name} uploaded successfully`);
+        } else if (info.file.status === "error") {
+            message.error(`${info.file.name} upload failed`);
+        }
+
+        let fileList = [...info.fileList];
+
+        fileList = fileList.map((file) => {
+            if (file.response) {
+                file.url = URLroot + file.response.filePath;
+                file.uid = file.response.fileKey;
+            }
+            return file;
+        });
+
+        setFileList(fileList);
+    };
+
     const uploadProps = {
         name: "file",
-        action: `${URLroot}/projects/upload-file/${projectId}`,
-        defaultFileList: files.map((file) => ({
-            name: file.fileName,
-            url: `${URLroot}/projects/get-file/${file.fileKey}`,
-        })),
-        //TODO
-        onRemove: () => {},
+        action: `${URLroot}/projects/${projectId}/upload-file`,
+        defaultFileList: initialFileList,
+        onRemove: async (file) => {
+            await showConfirm(file);
+        },
+        onChange: (info) => handleChange(info),
     };
 
     return (
-        <div style={{ maxWidth: "50%" }}>
+        <div style={{ maxWidth: "50%" }} fileList={fileList}>
             <Upload {...uploadProps}>
                 <Button icon={<UploadOutlined />}>Upload file</Button>
             </Upload>
